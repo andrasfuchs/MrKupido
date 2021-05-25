57c9854cb78e1de61312920b30068404189dd6ad
Andras Fuchs <andras.fuchs@gmail.com>
2021-05-25 12:54:02 +0200

    Update a few outdated package

--- Source/MrKupido.Web/Scripts/_references.js
+++ Source/MrKupido.Web/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.5.1.js" />
+﻿/// <reference path="jquery-3.6.0.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="jquery-ui-1.9.1.custom.js" />
 /// <reference path="jquery.validate.js" />
